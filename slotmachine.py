4946974c28d06740b3c4f868ba3a56fe676f0cad
mXrky4 <ryanjoelraymond@gmail.com>
2024-08-02 16:22:38 +0800

    Adding 3 main functions of the slot machine

---
+++ slotmachine.py
@@ -0,0 +1,20 @@
+# Function Generates the row of the slot machine symbols
+def spin_row():
+    symbols = ['😁', '😎', '😆', '😏', '🤑' ]
+    pass
+
+# Function: Prints the row that easy to read
+def print_row():
+    pass
+
+# Function: Calculate the payout based on the row and bet
+def get_payout():
+    pass
+
+
+
+
+# Game start
+def main():
+    pass
+
